a6f19b9b80691b17edbe43b49a7894c89bccb353
Rodrigo <rodrigo@domper.com.br>
2020-11-20 20:22:36 -0300

    Feito alterações

---
+++ src/CamadaCore/Context/CadastrosBasicoContext/ViewModels/Outputs/Paradas/ParadasResumoMotivoInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CamadaCore.Context.CadastrosBasicoContext.ViewModels.Outputs.Paradas
+{
+    public class ParadasResumoMotivoInputModel
+    {
+        public decimal Total { get; set; }
+        public string Nome { get; set; }
+        public decimal Percentual { get; set; }
+        public int Quant { get; set; }
+    }
+}
